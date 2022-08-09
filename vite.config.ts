220158bc9c5723cf69a75e5478c02f3d455f5a93
Nelson Loyola <nloyola@gmail.com>
2022-08-09 07:05:13 +0200

    For production builds, split the vendor code into its own chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { splitVendorChunkPlugin } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -9,7 +10,7 @@ export default defineConfig(({ command, mode }) => {
   const proxyTarget = `http://${env.BACKEND_SERVER}`;
 
   return {
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
     server: {
       proxy: {
         '/api': {
